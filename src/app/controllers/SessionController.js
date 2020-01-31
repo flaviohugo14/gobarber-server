9a3a9db828a86abde2a22e2fd0791b09744b9098
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-01-31 16:11:51 -0300

    feature/validation entry dates

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
 import auth from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fail' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
