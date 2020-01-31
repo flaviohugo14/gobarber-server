b0f60df12f500bbebfc977096da0809af2b7fae5
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-01-31 12:31:29 -0300

    config devDependencies and migration of users table

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
