7dca0b403f8e72ca7e47c6b5d9b83a993aa0457f
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-02-02 15:20:03 -0300

    create feature for send mail

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a5bc76afb04b21',
+    pass: 'a6549d217dc7ab',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ *  Amazon SES
+ *  Mailgun
+ *  SparkPost
+ *  Mailtrap (dev)
+ */
